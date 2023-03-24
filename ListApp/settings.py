0bb5ebd47c1ba64a792337c64af1021be8748b55
bmarquis3500 <bmarquis3500@gmail.com>
2023-03-24 11:54:52 -0400

    11 commit

--- ListApp/settings.py
+++ ListApp/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'ListApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ListApp',
+        'USER': 'postgres',
+        'PASSWORD': 'DontForgetThis1!',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
