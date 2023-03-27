7d9301b8f975bc9274839d072b5393df43405ad5
bmarquis3500 <bmarquis3500@gmail.com>
2023-03-27 07:38:35 -0400

    20 commit

--- ListApp/settings.py
+++ ListApp/settings.py
@@ -12,17 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
-import django_heroku
-import dj_database_url
-import dotenv
-from django.core.management.commands.runserver import Command as runserver
-
-
-runserver.default_port = '0.0.0.0' 
-
-dotenv_file = os.path.join(BASE_DIR,".env")
-if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,7 +128,6 @@ USE_TZ = True
 #STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
